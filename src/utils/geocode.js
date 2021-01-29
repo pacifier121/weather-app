ab22ac302189eca53b21d4b51f952eafe6d9f903
pacifier <prashantrawat2mx@gmail.com>
2021-01-29 21:00:41 +0530

    Fixed some issue

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const geocode = (place, callback) => {
-    let url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(place) + ".json?access_token=pk.eyJ1IjoicGFjaWZpZXIxMjEiLCJhIjoiY2trN2E3Mmk0MGMyMDJubXZ5aDE4ZmE0dCJ9.edddL0DzHyBjr2yLXBOnUg&limit=1";
+    let url = "https://cors-anywhere.herokuapp.com/https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(place) + ".json?access_token=pk.eyJ1IjoicGFjaWZpZXIxMjEiLCJhIjoiY2trN2E3Mmk0MGMyMDJubXZ5aDE4ZmE0dCJ9.edddL0DzHyBjr2yLXBOnUg&limit=1";
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
